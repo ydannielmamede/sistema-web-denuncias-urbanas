365905176d69deec96807ebe5523da2fe5da718b
Daniel Mamede <mamededaniel01@gmail.com>
2026-05-25 11:10:58 -0300

    feat: update database settings to use MySQL and enhance models for denuncia, orgao_alvo, perfil, and usuario

--- denuncia/models.py
+++ denuncia/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Categoria(models.Model):
-    id_perfil = models.AutoField(primary_key=True, null=False)
-    descricao_perfil = models.CharField(max_length=20, null=False, unique=False)
+class Denuncia(models.Model):
+    id_denuncia = models.AutoField(primary_key=True, null=False, db_column='id_denuncia')
+    mensagem = models.CharField(max_length=255, null=False, unique=False, db_column='Mensagem')
+    foto_video = models.CharField(max_length=100, null=True, unique=False, db_column='Foto_video')
+    data_hora = models.DateTimeField(auto_now_add=True, null=False, unique=False, db_column='Data_hora')
+    anonimo = models.BooleanField(default=False, null=False, unique=False, db_column='Anonimo')
+    localizacao = models.CharField(max_length=100, null=True, unique=False, db_column='Localizacao')
+    id_categoria = models.ForeignKey('usuario.Categoria', on_delete=models.CASCADE, db_column='CATEGORIA_id_categoria')
+    id_orgao_alvo = models.ForeignKey('orgao_alvo.OrgaoAlvo', on_delete=models.CASCADE, db_column='ORGAO_ALVO_id_orgao_alvo')
+    id_usuario = models.ForeignKey('usuario.Usuario', on_delete=models.CASCADE, db_column='USUARIO_id_usuario')
 
-class Meta:
-    db_table = 'perfil'
-    verbose_name = 'Perfil'
-    plural_verbose_name = 'Perfis'
+    class Meta:
+        db_table = 'denuncia'
+        verbose_name = 'Denuncia'
+        verbose_name_plural = 'Denuncias'
 
-def __str__(self):
-    return self.descricao_perfil
+    def __str__(self):
+        return self.mensagem
