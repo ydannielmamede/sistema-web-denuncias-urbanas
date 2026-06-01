f82df06df8a141714bacba549031fc7e76ad9c30
Daniel Mamede <mamededaniel01@gmail.com>
2026-06-01 11:54:41 -0300

    feat: Implement login form with floating labels and validation
    - Added new login URL in urls.py
    - Created login.html template
    - Developed form-utils.js for form validation and notification handling
    - Implemented login.js for managing login form behavior
    - Added CSS styles for the login form in login.css
    - Included success and error message handling in the login process
    - Introduced social login buttons for Google and GitHub

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('saibamais/', TemplateView.as_view(template_name='saibamais.html'), name='saibamais'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
 ]
