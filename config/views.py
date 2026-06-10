a1261fd15aff028bba02589440174ceb27e14d65
Daniel Mamede <mamededaniel01@gmail.com>
2026-06-10 12:01:54 -0300

    Add 'icone' field to Categoria model; update index view and template to display recent denuncias

--- config/views.py
+++ config/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from denuncia.models import Denuncia
 
 def index(request):
-    return render(request, 'index.html')
+    denuncias_recentes = Denuncia.objects.all().order_by('-data_hora')[:5]
+    return render(request, 'index.html', {
+        'denuncias_recentes': denuncias_recentes,
+    })
 
 
