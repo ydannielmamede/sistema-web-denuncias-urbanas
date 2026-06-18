04400f9ebefef6f20d075996da262916eee1945e
dannielmamede <dannielmamede@gmail.com>
2026-06-18 20:00:30 -0300

    Daniel denuncias app (#28)
    * Visualização das denúncias feitas
    
    * Aprimora listagem e seeds de denúncias
    
    ---------
    
    Co-authored-by: Daniel Mamede <mamededaniel01@gmail.com>

--- denuncia/urls.py
+++ denuncia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     criar_denuncia,
     denuncia_page,
+    listar_denuncias,
     marcar_status_em_analise,
     marcar_status_pendente,
     marcar_status_resolvida,
@@ -12,6 +13,7 @@ app_name = 'denuncia'
 urlpatterns = [
     path('', denuncia_page, name='denuncia'),
     path('criar/', criar_denuncia, name='criar_denuncia'),
+    path('denuncias/', listar_denuncias, name='listar_denuncias'),
     path('<int:id_denuncia>/status/pendente/', marcar_status_pendente, name='marcar_status_pendente'),
     path('<int:id_denuncia>/status/em-analise/', marcar_status_em_analise, name='marcar_status_em_analise'),
     path('<int:id_denuncia>/status/resolvida/', marcar_status_resolvida, name='marcar_status_resolvida'),
