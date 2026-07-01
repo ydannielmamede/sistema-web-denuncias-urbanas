8240d9cdb74976aa767eb39f1f4c680fd4a68ca5
Daniel Mamede <mamededaniel01@gmail.com>
2026-07-01 13:29:58 -0300

    Permite configurar backend de email

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,10 @@ from decouple import config
 
 import dj_database_url
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = config(
+    "EMAIL_BACKEND",
+    default="django.core.mail.backends.smtp.EmailBackend",
+)
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
